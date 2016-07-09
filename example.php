d9d116a47b59812b8f1819c411fda492e83e77b8
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-07-09 15:16:59 +0200

    Update

--- example.php
+++ example.php
@@ -5,8 +5,8 @@ require('lib/danog/PHP/Struct.php');
 $struct = new danog\PHP\Struct();
 
 
-var_dump($struct->unpack(">2cxbBx?xhxHxixIxlxLxqxQxnxNxfxdx2sx", 
-	   $struct->pack(">2cxbBx?xhxHxixIxlxLxqxQxnxNxfxdx2sx", 
+var_dump($struct->unpack(">2cxbxBx?xhxHxixIxlxLxqxQxnxNxfxdx2sx", 
+	   $struct->pack(">2cxbxBx?xhxHxixIxlxLxqxQxnxNxfxdx2sx", 
 "nv", 61, 61, false, 333, 444, 232423, 234342, 243342423424, 234234234234, 234234234234, 234234234234, 34434, 344434, 2.2343, 3.03424, "dd"
 
 )
