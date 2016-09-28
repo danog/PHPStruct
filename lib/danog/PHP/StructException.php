146ac3d89eb604c973280ad251c13f5b63e753ba
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-09-28 02:45:06 +0200

    Disable debugging

--- lib/danog/PHP/StructException.php
+++ lib/danog/PHP/StructException.php
@@ -18,9 +18,10 @@ class StructException extends \Exception
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         // some code
+        /*
         if (isset($GLOBALS['doingphptests']) && $GLOBALS['doingphptests']) {
             var_dump($message);
-        }
+        }*/
 
         // make sure everything is assigned properly
         parent::__construct($message, $code, $previous);
